c89a85bb66f5e7b8ff44b36dc0f390a01d001538
younho <nnyyhh9247@gmail.com>
2022-04-03 23:22:39 +0900

    [추가] coin api set

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,55 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.accentColor};
+  font-size: 28px;
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  display: block;
+`;
+
+const Rank = styled.div`
+  color: ${props => props.theme.accentColor};
+  font-size: 28px;
+  margin-left: 20px;
+`;
+
+interface RouteState {
+  name: string;
+  rank: number;
+}
 
 const Coin = () => {
   const { coinId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const { name, rank } = useLocation().state as RouteState;
 
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{name ? `Coin: ${name}` : "Loading..."}</Title>
+        <Rank>Coin Rank: {rank}</Rank>
+      </Header>
+      {loading ? <Loader>loading...</Loader> : null}
+    </Container>
+  );
 };
 
 export default Coin;
